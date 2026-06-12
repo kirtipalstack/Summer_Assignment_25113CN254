83b27d172e53ba9e45ae26645017e4b0ec049abb
Kirti Pal <palk23577@gmail.com>
2026-06-12 16:36:02 +0530

    Refactor Armstrong Number program to handle any digits

--- Day4/ArmstrongNumber.c
+++ Day4/ArmstrongNumber.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(){
-    int num,temp,rem,sum=0;
+    int num,digits=0,temp,rem,sum=0;
     printf("Enter a number :");
     scanf("%d",&num);
     temp=num;
-    while(temp!=0){
+    while(temp>0){
+        digits++;
         rem=temp%10;
-        sum=sum+(rem*rem*rem);
+        sum=sum+pow(rem,digits);
         temp=temp/10;
     }
     if(sum==num)
