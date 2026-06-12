166ff81a9f461cfed2a002a13d4d5e23c6f214a8
Kirti Pal <palk23577@gmail.com>
2026-06-12 16:48:20 +0530

    Fix prime number checking logic and variable names

--- Day3/prime.c
+++ Day3/prime.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
-    int i,nisPrime = 1;
+    int i,n,isPrime = 1;
     printf("enter a value :");
     scanf("%d",&n);
     if(num <= 1) {
         isPrime = 0;
     } else {
-  for(i=2;i<=n-1;i++){
-   if (num % i == 0) {
-                isPrime = 0;
-                break;
+  for(i=2;i<=n;i++){
+   if (n % i == 0) {
+            isPrime = 0;
+            break;
             }
         }
     }
